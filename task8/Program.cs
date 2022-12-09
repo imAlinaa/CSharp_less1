823f9d8ab4a5ff734e4bc2d6d69548302a9fb0f5
imAlinaa <alina.btkv@yandex.ru>
2022-12-09 20:10:06 +0700

    Задачи 2, 4, 6, 8

---
+++ task8/Program.cs
@@ -0,0 +1,12 @@
+﻿int i;
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for (i = 1; i <= a; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i} ");
+    }
+}
